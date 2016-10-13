40e50e866be07560e41e36efe1484e6c4dbf2a54
jsadler2 <jeffrey.sadler2@gmail.com>
2016-10-13 11:47:20 -0400

    doesn't add duplicates

--- get_server_data.py
+++ get_server_data.py
@@ -41,26 +41,25 @@ def parse_data(soup, map, data_tag, val_tag, in_tag, station_id, gw=False):
     return df
 
 
-def entry_exists(typ, con, code):
+def entry_exists(typ, con, code, **kwargs):
     cur = con.cursor()
     sql = """Select 1 FROM {}s WHERE {}Code = "{}" """.format(typ.lower(), typ, code)
-    if typ == 'datavalues':
-        val, datetime, variable_id, site_id = code
-        sql = """Select 1 FROM {} WHERE Datetime={} AND VariableID={} AND SITEID={}""".format(
-            typ,
-            datetime,
-            variable_id,
-            site_id
-        )
+    if typ == 'Variable':
+        sql = """Select 1 FROM variables WHERE VariableCode  = "{}"
+                                    AND VariableType = "{}" """\
+            .format(code, kwargs['variable_type'])
 
     res = cur.execute(sql)
 
     return len(res.fetchall())
 
 
-def get_id(typ, con, soup, code):
-    if entry_exists(typ, con, code):
+def get_id(typ, con, soup, code, **kwargs):
+    if entry_exists(typ, con, code, **kwargs):
         sql = """SELECT {0}ID FROM {1}s WHERE {0}Code = "{2}" """.format(typ, typ.lower(), code)
+        if typ == "Variable":
+            sql = """SELECT {0}ID FROM {1}s WHERE {0}Code = "{2}" AND
+                        {0}Type = "{3}" """.format(typ, typ.lower(), code, kwargs['variable_type'])
         cur = con.cursor()
         res = cur.execute(sql)
         id = res.next()[0]
@@ -70,7 +69,7 @@ def get_id(typ, con, soup, code):
         data = get_data_fxn(soup)
         create_fxn = globals()["create_{}".format(typ.lower())]
         create_fxn(con, data)
-        return get_id(typ, con, soup, code)
+        return get_id(typ, con, soup, code, **kwargs)
 
 
 def create_data_value(con, datavalue):
@@ -111,8 +110,10 @@ def parse_wml2_data(wml2url):
     variable_block = soup.find_all('wml2:observationmember')
     for v in variable_block:
         value_tags_list = v.find_all('wml2:point')
-        variable_code = get_variable_data(v)[0]
-        variable_id = get_id('Variable', con, soup, variable_code)
+        variable_data = get_variable_data(v)
+        variable_code = variable_data[0]
+        variable_type = variable_data[2]
+        variable_id = get_id('Variable', con, v, variable_code, variable_type=variable_type)
         for value_tag in value_tags_list:
             datetime = value_tag.find('wml2:time').text
             val = value_tag.find('wml2:value').text
@@ -125,6 +126,9 @@ def parse_wml2_data(wml2url):
     df = pd.DataFrame(res_list)
     df = make_date_index(df, 'Datetime')
     df['Value'] = pd.to_numeric(df['Value'])
+
+    # check against current datavalues table in db to avoid storing duplicates
+    df = remove_duplicates(con, df)
     df.to_sql('datavalues', con, if_exists='append')
     return df
 
@@ -146,7 +150,7 @@ def get_variable_data(soup):
 
 
 def make_date_index(df, field):
-    df[field] = pd.DatetimeIndex(df[field])
+    df.loc[:, field] = pd.DatetimeIndex(df.loc[:, field])
     df.set_index(field, drop=True, inplace=True)
     return df
 
@@ -154,3 +158,33 @@ def make_date_index(df, field):
 def save_as_sqlite(df, table_name):
     con = sqlite3.connect('data.sqlite')
     df.to_sql(table_name, con)
+
+
+def remove_duplicates(con, df):
+    db_df = get_db_table_as_df(con, 'datavalues')
+    if not db_df.empty:
+        df.reset_index(inplace=True)
+        db_df.reset_index(inplace=True)
+        merged = df.merge(db_df,
+                          how='outer',
+                          on=['SiteID', 'VariableID', 'Datetime'],
+                          indicator=True)
+        non_duplicated = merged[merged._merge == 'left_only']
+        non_duplicated = make_date_index(non_duplicated, 'Datetime')
+        non_duplicated.loc[:, 'Value'] = non_duplicated['Value_x']
+        non_duplicated = non_duplicated[['SiteID', 'VariableID', 'Value']]
+        return non_duplicated
+    else:
+        return df
+
+
+def get_db_table_as_df(con, name):
+    sql = """SELECT * FROM {};""".format(name)
+    if name == 'datavalues':
+        date_col = 'Datetime'
+    else:
+        date_col = None
+    df = pd.read_sql(sql, con, parse_dates=date_col)
+    if name == 'datavalues':
+        df = make_date_index(df, 'Datetime')
+    return df
