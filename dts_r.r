14e77d0b9ba257a9ba0b41e76028392d10c533a6
jsadler2 <jeffrey.sadler2@gmail.com>
2017-04-20 15:35:52 -0400

    added tide at rain maxes for all days

--- dts_r.r
+++ dts_r.r
@@ -4,6 +4,7 @@ library(rpart)
 library(rpart.plot)
 library(RSQLite)
 library(DBI)
+library(randomForest)
 
 get_nn = function(d, point, k){
   point = d[point, ]
@@ -24,15 +25,13 @@ dbListTables(con)
 df = dbReadTable(con, 'for_model')
 df = subset(df, rain_daily_sum>0.1) # only look at days that have over 0.1 in of rainfall
 df = df[!is.na(df$gw_daily_avg),] # remove null gw values
-short_names = c('rd', 'rhr', 'r15', 'r3d', 'gw', 'td', 'wdd', 'wvd',  'wvh', 'nfld', 'fld', 'evnme', 'evdte')
+short_names = c('rd', 'rhr', 'rhmxtime', 'r15', 'r15mxtime', 'r3d', 'gw', 'td', 'tr15mx', 'trhrmx', 'wdd', 'wvd',  'wvh', 'nfld', 'fld', 'evnme', 'evdte')
 colnames(df) = short_names
 
-in_cols = c(1:6, 8)
-in_col_names = colnames(df)[in_cols]
+in_col_names = c('rd', 'rhr', 'r15', 'r3d', 'gw', 'td', 'tr15mx', 'trhrmx', 'wvd')
 out_col_name = 'fld'
 data = df[, c(in_col_names, out_col_name)]
-#data = data.frame(scale(data))
-data$fld = data$fld>0
+#data$fld = df$fld>0
 
 fmla = as.formula(paste(out_col_name, "~", paste(in_col_names, collapse="+")))
 
@@ -42,6 +41,9 @@ tiff(paste(fig_dir, "Plot2.tif"), width=9, height=6, units='in', res = 300)
 rpart.plot(fit, under=TRUE, cex=0.9, extra=1, varlen = 6)
 dev.off()
 rpart.plot(fit, under=TRUE, cex=0.9, extra=1, varlen = 6)
+
+# pca
+data = data.frame(scale(data))
 pca = prcomp(data)
 p = ggplot(pca$x[,c(1,2)], aes(x=PC1, y=PC2, colour=data[, out_col_name], label=rownames(pca$x)))
 p + geom_point() +geom_text()
@@ -53,7 +55,7 @@ data[neighs,]
 data$fld = factor(data$fld)
 prt = createDataPartition(data$fld, p=0.7)
 train_ind = prt$Resample1
-forest = randomForest(fmla, data = data[train_ind, ], importance = TRUE, type="classification")
+forest = randomForest(fmla, data = data, importance = TRUE, type="classification")
 
 # check on training data
 pred = predict(forest, data[train_ind, in_col_names])
