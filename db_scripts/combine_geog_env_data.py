3342c2cc78e7c5593224a25bcd11355dc7c44cf0
jsadler2 <jeffrey.sadler2@gmail.com>
2017-05-11 12:19:21 -0400

    added non-flooding locations fancier geog data to model

--- db_scripts/combine_geog_env_data.py
+++ db_scripts/combine_geog_env_data.py
@@ -19,7 +19,6 @@ daily_obs = get_db_table_as_df('dntwn_nor_daily_observations', date_col=['Dateti
 
 # In[3]:
 
-flood_locations.set_index('location', inplace=True)
 flood_locations.head()
 
 
@@ -31,59 +30,73 @@ daily_obs.head()
 
 # In[5]:
 
-flood_events.head()
+daily_obs.shape
 
 
 # In[6]:
+
+flood_events.head()
+
+
+# In[7]:
 
 for_model.set_index('event_date', inplace=True)
 for_model.head()
 
 
-# In[7]:
-
-dwntwn_locations = flood_locations[flood_locations['is_downtown']==1].index
-
-
 # In[8]:
 
-lst = []
-for l in dwntwn_locations:
-    event_dates = flood_events[flood_events['location'] == l]['event_date']
-    all_fld_dates = flood_events[flood_events['event_date'].isin(event_dates)]['dates']
-    all_fld_dates = np.append(all_fld_dates, event_dates)
-    all_fld_dates = pd.to_datetime(np.unique(all_fld_dates))
-
-    fld_data = for_model[for_model.index.isin(event_dates)]
-    
-    loc_data = daily_obs.copy()
-    loc_data = loc_data[~loc_data.index.isin(all_fld_dates)]
-    loc_w_fld = pd.concat([loc_data, fld_data])
-    
-    # add geog data
-    loc_w_fld['location'] = l
-    geog_data = flood_locations.loc[l, :]
-    for k in geog_data.keys():
-        loc_w_fld[k] = geog_data[k]
-    lst.append(loc_w_fld)
-        
+flood_locations.shape
 
 
 # In[9]:
+
+lst = []
+for index, row in flood_locations.iterrows():
+    l = row['location'].strip()
+    loc_data = daily_obs.copy()
+    loc_data['flooded'] = 0
+    if l != '':
+        event_dates = flood_events[flood_events['location'] == l]['event_date']
+        all_fld_dates = flood_events[flood_events['event_date'].isin(event_dates)]['dates']
+        all_fld_dates = np.append(all_fld_dates, event_dates)
+        all_fld_dates = pd.to_datetime(np.unique(all_fld_dates))
+
+        fld_data = for_model[for_model.index.isin(event_dates)]
+    
+        loc_data = loc_data[~loc_data.index.isin(all_fld_dates)]
+        loc_data = pd.concat([loc_data, fld_data])
+    
+        # add geog data
+    for k in row.keys():
+        loc_data[k] = row[k]
+    lst.append(loc_data)
+#         print l
+#         print loc_data.head()
+        
+
+
+# In[10]:
 
 all_locations = pd.concat(lst)
 all_locations.reset_index(inplace=True)
 
 
-# In[10]:
+# In[11]:
+
+all_locations.shape
+
+
+# In[12]:
 
 new_cols = all_locations.columns.tolist()
 new_cols[0] = 'Date'
 all_locations.columns = new_cols
-all_locations.head()
+print all_locations.shape
+all_locations.tail()
 
 
-# In[11]:
+# In[13]:
 
 con = sqlite3.connect(db_filename)
 all_locations.to_sql(con=con, name='for_model_geog', if_exists='replace')
--- models/model_flood_occurrence.r
+++ models/model_flood_occurrence.r
@@ -2,6 +2,8 @@
 repo = "http://cran.us.r-project.org"
 library(caret)
 library(ggfortify)
+library(ggplot2)
+library(dplyr)
 library(rpart)
 library(rpart.plot)
 library(RSQLite)
@@ -30,23 +32,58 @@ db_filename <- "floodData.sqlite"
 
 con = dbConnect(RSQLite::SQLite(), dbname=paste(data_dir, db_filename, sep=""))
 
-df = dbReadTable(con, 'for_model')
-df = subset(df, rain_daily_sum>0.1) # only look at days that have over 0.1 in of rainfall
-df = df[!is.na(df$gw_daily_avg),] # remove null gw values
-short_names = c('rd', 'rhr', 'rhmxtime', 'r15', 'r15mxtime', 'r3d', 'gw', 'td', 'tr15mx', 'trhrmx', 'wdd', 'wvd',  'wvh', 'nfld', 'fld', 'evnme', 'evdte')
-colnames(df) = short_names
+df = dbReadTable(con, 'for_model_geog')
 
-in_col_names = c('rd', 'rhr', 'r15', 'r3d', 'gw', 'td', 'wvd', 'tr15mx', 'trhrmx')
-out_col_name = 'fld'
+df = df[df$is_downtown == 1,]
+df = df[!is.na(df$gw_daily_avg),]
+df = subset(df, rain_daily_sum>0.1)
+
+colnames(df)
+
+in_col_names = c('rain_daily_sum',
+                 'rain_hourly_max',
+                 'rain_15_min_max', 
+                 'rain_prev_3_days', 
+                 'gw_daily_avg', 
+                 'tide_daily_avg', 
+                 'wind_vel_daily_avg',
+                 'tide_r15mx', 
+                 'tide_rhrmx',
+#                  'distance_to_OW',
+                 'num_imperv',
+                 'disttowat',
+                 'elev'
+                )
+out_col_name = 'flooded'
 model_data = df[, c(in_col_names, out_col_name)]
-model_data$fld = df$fld>0
-model_data$fld = factor(model_data$fld)
+model_data[,out_col_name] = model_data[,out_col_name]>0
+model_data[,out_col_name] = factor(model_data[,out_col_name])
 
-prt = createDataPartition(model_data$fld, p=0.7)
+head(model_data)
+
+print(summary(model_data$flooded))
+sum(as.numeric(model_data$flooded == TRUE))/sum(as.numeric(model_data$flooded == FALSE))
+
+prt = createDataPartition(model_data[, out_col_name], p=0.7)
 train_ind = prt$Resample1
+test_ind = rownames(model_data[-train_ind,])
+print(length(test_ind) + length(train_ind))
+# length(rownames(model_data[model_data[train_ind, out_col_name]==TRUE]))
+length(test_ind)
+
+train_data = model_data[train_ind, ]
+test_data = model_data[test_ind, ]
+
+nfld_train = rownames(train_data[train_data[, out_col_name] == FALSE,])
+fld_train = rownames(train_data[train_data[, out_col_name] == TRUE,])
+nfld_test = rownames(test_data[test_data[, out_col_name] == FALSE,])
+fld_test = rownames(test_data[test_data[, out_col_name] == TRUE,])
+down_sample_percent = .002
+nfld_train_dwnsmp = sample(nfld_train, down_sample_percent*length(nfld_train))
+train_ind = c(fld_train, nfld_train_dwnsmp)
 
 train_in_data = model_data[train_ind, in_col_names]
-test_in_data = model_data[-train_ind, in_col_names]
+test_in_data = model_data[test_ind, in_col_names]
 
 train_col_stds = apply(train_in_data, 2, sd)
 train_col_means = colMeans(train_in_data)
@@ -59,17 +96,17 @@ pca$x = -pca$x
 pca$rotation=-pca$rotation
 p = ggplot(pca$x[,c(1,2)], aes(x=PC1, y=PC2, colour=model_data[train_ind, out_col_name], label=rownames(pca$x)))
 p + geom_point() + geom_text()
-plot(pca)
 print(pca)
+plot(pca)
 
 trn_preprocessed = predict(pca, train_normalized)
 tst_preprocessed = predict(pca, test_normalized)
 trn_in = trn_preprocessed
 tst_in = tst_preprocessed
 
-tst_out = model_data[-train_ind, 'fld']
-trn_out = model_data[train_ind, 'fld']
-train_data = cbind(as.data.frame(trn_in), fld = model_data[train_ind, 'fld'])
+tst_out = model_data[test_ind, out_col_name]
+trn_out = model_data[train_ind, out_col_name]
+train_data = cbind(as.data.frame(trn_in), flooded = model_data[train_ind, out_col_name])
 fmla = as.formula(paste(out_col_name, "~", paste(colnames(trn_in), collapse="+")))
 fmla
 
@@ -82,7 +119,7 @@ table(tst_out, svm_pred)
 
 dt_fmla = as.formula(paste(out_col_name, "~", paste(in_col_names, collapse="+")))
 dt_train_data = model_data[train_ind, ]
-dt_test_data = model_data[-train_ind, in_col_names]
+dt_test_data = model_data[test_ind, in_col_names]
 
 fit = rpart(dt_fmla, method='class', data=dt_train_data, minsplit=2)
 printcp(fit)
@@ -101,14 +138,28 @@ table(trn_out, pred)
 pred = predict(pfit, dt_test_data, type = 'class')
 table(tst_out, pred)
 
-forest = randomForest(fmla, data = train_data, importance = TRUE, type="classification", nodesize=2)
+forest = randomForest(dt_fmla, data = dt_train_data, importance = TRUE, type="classification", nodesize=2)
 
-pred = predict(forest, trn_in)
+pred = predict(forest, dt_train_data[, in_col_names])
 table(trn_out, pred)
 
-pred = predict(forest, tst_in)
+pred = predict(forest, dt_test_data)
 table(tst_out, pred)
-forest$importance
+var_importance <- data.frame(variable=setdiff(colnames(dt_train_data), out_col_name),
+                             importance=as.vector(importance(forest)))
+var_importance <- arrange(var_importance, desc(importance))
+var_importance$variable <- factor(var_importance$variable, levels=var_importance$variable)
+
+p <- ggplot(var_importance, aes(x=variable, weight=importance, fill=variable))
+p <- p + geom_bar() + ggtitle("Variable Importance from Random Forest Fit")
+p <- p + xlab("") + ylab("Variable Importance (Mean Decrease in Gini Index)")
+p <- p + scale_fill_discrete(name="Variable Name")
+p + theme(axis.text.x=element_blank(),
+          axis.text.y=element_text(size=12),
+          axis.title=element_text(size=16),
+          plot.title=element_text(size=18),
+          legend.title=element_text(size=16),
+          legend.text=element_text(size=12))
 
 lo_fit = glm(fmla, family=binomial(link='logit'), data=train_data)
 print(lo_fit)
