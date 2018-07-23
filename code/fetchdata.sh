87b6870e97e8065a3a515ccb2e6bd672c6ab1581
Jennifer Chang <jenchang@iastate.edu>
2018-07-23 18:16:30 -0400

    Reorg and fetching data
    * code and data folders added
    * added fetchdata.sh to pull 10 datasets from hydroshare

---
+++ code/fetchdata.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+# Auth: Jennifer Chang
+# Date: 2018/07/23
+
+set -e
+set -u
+
+wget https://www.hydroshare.org/django_irods/download/bags/9e1b23607ac240588ba50d6b5b9a49b5.zip
+wget https://www.hydroshare.org/django_irods/download/bags/54df00b15c02458685fa3b622f2ecc7b.zip
+wget https://www.hydroshare.org/django_irods/download/bags/ff8be5aea3224c15b262bfddd5fb6033.zip
+wget https://www.hydroshare.org/django_irods/download/bags/712cd2ce8f604c8f824d6836ee3fcb53.zip
+wget https://www.hydroshare.org/django_irods/download/bags/77314eb264c84bb6833cf19e8ade03a9.zip
+wget https://www.hydroshare.org/django_irods/download/bags/38a4ce62960942b4ad8398ee58a777cf.zip
+wget https://www.hydroshare.org/django_irods/download/bags/fb2775104b554f4da0dc1045192caf6f.zip
+wget https://www.hydroshare.org/django_irods/download/bags/5db7884111fb4662a13f64707c0c6890.zip
+wget https://www.hydroshare.org/django_irods/download/bags/6c9e9239f70a421b8c49ae642a7b9291.zip
+wget https://www.hydroshare.org/django_irods/download/bags/41c8d8f8788c4ba0b0bfbb924fe1d403.zip
+wget https://www.hydroshare.org/django_irods/download/bags/50c26ff32a1f461090a36eb0099dad76.zip
+wget https://www.hydroshare.org/django_irods/download/bags/b56fea2856bc468bb0e32fc0ad8f910e.zip
