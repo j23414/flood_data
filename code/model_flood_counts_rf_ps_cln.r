205f177bf43039a81a6dea55049fd20d5f7e6c7e
AliceTianaiSun <41303544+AliceTianaiSun@users.noreply.github.com>
2018-07-26 11:10:00 -0400

    Update model_flood_counts_rf_ps_cln.r

--- code/model_flood_counts_rf_ps_cln.r
+++ code/model_flood_counts_rf_ps_cln.r
@@ -62,7 +62,7 @@ clear_db_tables = function(models, con, suffix){
 }
 
 
-df = read.csv('for_model_avgs.csv', )
+df = read.csv('../data/for_model_avgs.csv', )
 
 colnames(df)
 
@@ -108,8 +108,8 @@ for (i in 1:101){
 	  colnames(all_trn_df) = c('all_trn', 'all_pred_trn')
 	  all_tst_df = data.frame(test_out_data, unlist(pred_test))
 	  colnames(all_tst_df) = c('all_tst', 'all_pred_tst')
-	  write.table(all_trn_df, paste(model, '_', suffix, '_train.csv', sep=""), append=TRUE,  sep=",", col.names = F)
-	  write.table(all_tst_df, paste(model, '_', suffix, '_test.csv', sep=""), append=TRUE,  sep=",", col.names = F)
+	  write.table(all_trn_df, paste("../data/",model, '_', suffix, '_train.csv', sep=""), append=TRUE,  sep=",", col.names = F)
+	  write.table(all_tst_df, paste("../data/",model, '_', suffix, '_test.csv', sep=""), append=TRUE,  sep=",", col.names = F)
 
 	  if (model == 'rf'){
       impo = model_results[3]
@@ -120,4 +120,4 @@ for (i in 1:101){
 
 colnames(import_df) = 1:ncol(import_df)
 row.names(import_df) = in_col_names
-write.csv(import_df, paste('rf_impo_', suffix, sep=""), append=TRUE)
+write.csv(import_df, paste('../data/rf_impo_', suffix, sep=""), append=TRUE)
