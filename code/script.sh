fd03b605e96f9a6117ded1127bdad8a3515da00e
AliceTianaiSun <41303544+AliceTianaiSun@users.noreply.github.com>
2018-07-26 13:18:37 -0400

    Update script.sh

--- code/script.sh
+++ code/script.sh
@@ -2,13 +2,13 @@
 # Auth: Jennifer Chang, Alice Sun, Matthew Manning, Shuting Yan
 # Date: 2018/07/25
 
-cd /flood_data/repro; python make_dly_obs_table_standalone.py
-cd /flood_data/repro; python prepare_flood_events_table.py
-cd /flood_data/repro; python by_event_for_model.py
-cd /flood_data/repro; Rscript model_flood_counts_rf_ps_cln.r
-cd /flood_data/repro; python plot_count_model_results.py out
+cd /flood_data/code; python make_dly_obs_table_standalone.py
+cd /flood_data/code; python prepare_flood_events_table.py
+cd /flood_data/code; python by_event_for_model.py
+cd /flood_data/code; Rscript model_flood_counts_rf_ps_cln.r
+cd /flood_data/code; python plot_count_model_results.py out
 
-cp /flood_data/repro/*.png /data/.
-cp /flood_data/repro/*.eps /data/.
-cp /flood_data/repro/*.csv /data/.
-cp /flood_data/repro/rf_impo_out /data/.
+cp /flood_data/data/*.png /data/.
+cp /flood_data/data/*.eps /data/.
+cp /flood_data/data/*.csv /data/.
+cp /flood_data/data/rf_impo_out /data/.
